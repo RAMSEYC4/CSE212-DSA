3020c8490bf676190bea315b13e731075430522c
RAMSEYC4 <ramseycedou68@gmail.com>
2026-01-09 21:05:53 +0200

    2026/01/09 end of week work

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -8,6 +8,14 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        var numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        foreach (var n in numbers)
+        {
+            Console.WriteLine(n);
+        }
     }
 }
